17f8c06a508dc1d8facfcee8ce16ea0042d7c660
Michael CORNILLON <contact@dwaps.fr>
2019-03-21 10:32:27 +0100

    style books list

--- src/app/shared/components/books/books-list/books-list.component.ts
+++ src/app/shared/components/books/books-list/books-list.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksListComponent implements OnInit {
 
+  books: any[] = [
+    {
+      id: 1,
+      title: 'Il était une fois',
+      price: 3.87,
+      copies: 16,
+      selected: true,
+      available: true
+    },
+    {
+      id: 2,
+      title: 'Raconte-moi',
+      price: 53,
+      copies: 0,
+      selected: false,
+      available: false
+    },
+    {
+      id: 3,
+      title: 'Biographie étrange',
+      price: 28,
+      copies: 6,
+      selected: false,
+      available: true
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
