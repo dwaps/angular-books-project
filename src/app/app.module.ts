ff2ee6b56ccba6b59169d867e685f3e214d17b31
Michael CORNILLON <contact@dwaps.fr>
2019-03-21 11:43:49 +0100

    correction : dossiers mal placés

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 // Components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { BooksComponent } from './shared/components/books/books.component';
 import { BooksListComponent } from './shared/components/books/books-list/books-list.component';
 import { BookDetailsComponent } from './shared/components/books/book-details/book-details.component';
-import { HeaderComponent } from './shared/components/shared/components/header/header.component';
-import { FooterComponent } from './shared/components/shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
